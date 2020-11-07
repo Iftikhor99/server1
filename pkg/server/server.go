e652624961253302defd4a0accd00c1b403b845f
Iftikhor99 <1987-2005@mail.ru>
2020-11-07 16:41:12 +0500

    changes added

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,13 +1,12 @@
 package server
 
 import (
+	"runtime"
 	"bytes"
 	"io"
-	"runtime"
-	"strconv"
 	"strings"
-
-	//	"fmt"
+//	"strconv"
+//	"fmt"
 	"log"
 	"net"
 	"sync"
@@ -26,6 +25,7 @@ type Server struct {
 	handlers map[string]HandlerFunc
 }
 
+
 //NewServer for
 func NewServer(addr string) *Server {
 	return &Server{addr: addr, handlers: make(map[string]HandlerFunc)}
@@ -40,24 +40,18 @@ func (s *Server) Register(path string, handler HandlerFunc) {
 
 }
 
+
 //Start for
 func (s *Server) Start() error {
 	// TODO: start server on host & port
-	//var wg sync.WaitGroup
-	log.Println(len(s.handlers))
-
+	var wg sync.WaitGroup
 	listener, err := net.Listen("tcp", s.addr)
 	if err != nil {
 		log.Print(err)
 		return err
 	}
-	if len(s.handlers) == 0 {
-		listener.Close()
-		log.Print("if no handlerrrrrrrrrrrrr")
-		return err
-	}
 	defer func() {
-		//	wg.Wait()
+		wg.Wait()
 		if cerr := listener.Close(); cerr != nil {
 
 			if err == nil {
@@ -73,18 +67,17 @@ func (s *Server) Start() error {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err)
-			conn.Close()
 			continue
 		}
-
-		//		for i := 0; i < len(s.handlers); i++ {
-
-		go s.handle(conn)
+		
+//		for i := 0; i < len(s.handlers); i++ {
+			wg.Add(1)
+			go s.handle(conn, &wg)
 		//	wg.Done()
-		log.Println("number of gorutines: ", runtime.NumGoroutine())
-
-		//		}
-
+			log.Println("number of gorutines: ", runtime.NumGoroutine())
+			
+//		}
+		
 		// if err != nil {
 		// 	log.Print(err)
 		// 	continue
@@ -92,22 +85,22 @@ func (s *Server) Start() error {
 
 	}
 
-	//	wg.Wait()
+	
 
 	return nil
 
 }
 
-func (s *Server) handle(conn net.Conn) {
+func (s *Server) handle(conn net.Conn, wg *sync.WaitGroup) {
 	var err error
 	//var wg sync.WaitGroup
 	//wg.Add(1)
 	//mu := s.mu
-	//	wg := sync.WaitGroup{}
-	//	defer wg.Done()
+//	wg := sync.WaitGroup{}
+//	defer wg.Done()
 	//wg.Add(1)
 	defer func() {
-		//	wg.Done()
+	//	wg.Done()
 		if cerr := conn.Close(); cerr != nil {
 			if err == nil {
 				err = cerr
@@ -116,81 +109,78 @@ func (s *Server) handle(conn net.Conn) {
 			log.Print(err)
 		}
 	}()
-	for {
-		buf := make([]byte, 4096)
 
-		n, err := conn.Read(buf)
-		if n == 0 || err == io.EOF {
-			log.Printf("%s", buf[:n])
-			log.Print(err)
-			log.Print("We are hereeeeeeeeeee")
-		//	conn.Close()
-			break
-		}
-		// if err != nil {
-		// 	log.Print(err)
-		// }
+	buf := make([]byte, 4096)
+
+	n, err := conn.Read(buf)
+	if err == io.EOF {
 		log.Printf("%s", buf[:n])
-
-		data := buf[:n]
-		requestLineDeLim := []byte{'\r', '\n'}
-		requestLineEnd := bytes.Index(data, requestLineDeLim)
-		if requestLineEnd == -1 {
-
-		}
-
-		requestLine := string(data[:requestLineEnd])
-		parts := strings.Split(requestLine, " ")
-		if len(parts) != 3 {
-
-		}
-
-		method, path, version := parts[0], parts[1], parts[2]
-
-		if method != "GET" {
-
-		}
-
-		if version != "HTTP/1.1" {
-
-		}
-		// if path == "/" {
-
-		// }
-
-		if path == "/" {
-			s.mu.RLock()
-			//		handler := s.handlers["/"]
-			//
-			s.mu.RUnlock()
-			//		handler(conn)
-
-			//	wg.Done()
-		}
-		//log.Print(npm)
-		body := "Ok!"
-		//	body, err := ioutil.ReadFile("static/index.html")
-
-		_, err = conn.Write([]byte(
-			"HTTP/1.1 200 OK\r\n" +
-				"Conect-Length: " + strconv.Itoa(len(body)) + "\r\n" +
-				"Content-Type: text/html	\r\n" +
-				"Connection: close\r\n" +
-				"\r\n" +
-				string(body),
-		))
-		if err != nil {
-			log.Print(err)
-		}
-
-		if path == "/about" {
-			s.mu.RLock()
-			//		handler := s.handlers["/about"]
-			//		handler(conn)
-			s.mu.RUnlock()
-			//		handler(conn)
-			//	wg.Done()
-		}
+		log.Print(err)
 	}
-	//	wg.Wait()
+	if err != nil {
+		log.Print(err)
+	}
+	log.Printf("%s", buf[:n])
+
+	data := buf[:n]
+	requestLineDeLim := []byte{'\r', '\n'}
+	requestLineEnd := bytes.Index(data, requestLineDeLim)
+	if requestLineEnd == -1 {
+
+	}
+
+	requestLine := string(data[:requestLineEnd])
+	parts := strings.Split(requestLine, " ")
+	if len(parts) != 3 {
+
+	}
+
+	method, path, version := parts[0], parts[1], parts[2]
+
+	if method != "GET" {
+
+	}
+
+	if version != "HTTP/1.1" {
+
+	}
+
+	if path == "/" {
+		s.mu.RLock()
+		handler := s.handlers["/"]
+		
+		s.mu.RUnlock()
+	//	handler(conn)
+		log.Print(handler)
+		wg.Done()
+	// 	//log.Print(npm)
+	// 	body := "Ok!"
+	// //	body, err := ioutil.ReadFile("static/index.html")
+	// 	if err != nil {
+	// 		return fmt.Errorf("can't read index.html: %w", err)
+	// 	}
+	// 	_, err = conn.Write([]byte(
+	// 		"HTTP/1.1 200 OK\r\n" +
+	// 			"Conect-Length: " + strconv.Itoa(len(body)) + "\r\n" +
+	// 			"Content-Type: text/html	\r\n" +
+	// 			"Connection: close\r\n" +
+	// 			"\r\n" +
+	// 			string(body),
+	// 	))
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	}
+	
+	if path == "/about" {
+		s.mu.RLock()
+		handler := s.handlers["/about"]
+		
+		s.mu.RUnlock()
+		//handler(conn)
+		log.Print(handler)
+		wg.Done()
+	}
+
+	wg.Wait()
 }
